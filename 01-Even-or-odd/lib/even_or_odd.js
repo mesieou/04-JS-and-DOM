fcc64c069395a73ec87a53156650042b47aaad81
juan bernal <mesieou@gmail.com>
2023-12-26 16:20:21 +1100

    even or odd exercise completed

--- 01-Even-or-odd/lib/even_or_odd.js
+++ 01-Even-or-odd/lib/even_or_odd.js
@@ -6,6 +6,11 @@ import runChallenges from "../spec/even_or_odd_examiner.js";
 
 export const evenOrOdd = (number) => {
   // TODO: this should return "even" if the number is even, "odd" otherwise
+  if (number % 2 === 0) {
+    return "even"
+  } else {
+    return "odd"
+  }
 };
 
 // Checking exercise answers. DO NOT MODIFY THIS LINE.
