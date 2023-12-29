476704016cb2b49da9d22110988870d9a16cfc6a
juan bernal <mesieou@gmail.com>
2023-12-29 12:22:50 +1100

    js puzzle - can move and move tile functions completed

--- 05-JS-puzzle/lib/puzzle.js
+++ 05-JS-puzzle/lib/puzzle.js
@@ -3,10 +3,28 @@ const tiles = document.querySelectorAll('td');
 
 const canMove = (tile) => {
   // TODO: Check if a tile has an empty neighbour
+  const tileRowIndex = tile.parentNode.rowIndex
+  const tileCellIndex = tile.cellIndex
+  const table = document.querySelector('tbody')
+  const upperCell = tileRowIndex == 0 ? undefined : table.rows[tileRowIndex - 1].cells[tileCellIndex]
+  const lowerCell = tileRowIndex >= 3 ? undefined : table.rows[tileRowIndex + 1].cells[tileCellIndex]
+  const leftCell = table.rows[tileRowIndex].cells[tileCellIndex - 1]
+  const rightCell = table.rows[tileRowIndex].cells[tileCellIndex + 1]
+
+  return (upperCell?.classList?.contains('empty') ||
+          lowerCell?.classList?.contains('empty') ||
+          leftCell?.classList?.contains('empty') ||
+          rightCell?.classList?.contains('empty')
+          )
 }
 
-const moveTile = (element) => {
+const moveTile = (tile) => {
   // TOOD: Move the tile
+  const emptyTile = document.querySelector('.empty')
+  emptyTile.textContent = tile.textContent
+  emptyTile.classList.toggle('empty')
+  tile.textContent = ''
+  tile.classList.toggle('empty')
 };
 
 const checkIfPlayerWins = () => {
